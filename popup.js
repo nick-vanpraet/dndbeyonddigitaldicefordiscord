3a0b271198b59eba3c7c71921a398afaa29ddc6c
Nick Vanpraet <nick.vanpraet@dropsolid.com>
2020-06-04 22:44:45 +0200

    form working, testing messaging between popup and (in)active tabs + focus loss/gain

--- popup.js
+++ popup.js
@@ -8,11 +8,11 @@ var template = `
 		  	<input type="checkbox" class="active" name="active-{index}" data-replace-check>
 		</fieldset>
 		<fieldset>
-		 	<label for="character_page">Character Page</label>
+		 	<label for="character_page">DnDBeyond Character Sheet URL</label>
 		  	<input type="url" class="character-page" name="character_page-{index}" required data-replace-char>
 		</fieldset>
 		<fieldset>
-		 	<label for="destination_url">Destination URL</label>
+		 	<label for="destination_url">Discord Webhook</label>
 		  	<input type="url" class="destination-url" name="destination_url-{index}" required data-replace-url>
 		</fieldset>
 		<fieldset>
@@ -23,98 +23,109 @@ var template = `
 
 
 function htmlToElement(html, i, data = {}) {
-	let e = document.createElement('template');
-	html = html.trim();
-	html = html.replace(/{index}/g, i)
+    let e = document.createElement('template');
+    html = html.trim();
+    html = html.replace(/{index}/g, i)
 
-	if (data.character) {
-		html = html.replace('data-replace-char', 'value="' + data.character + '"')
-	}
-	if (data.destination) {
-		html = html.replace('data-replace-url', 'value="' + data.destination + '"')
-	}
-	if (data.active) {
-		html = html.replace('data-replace-check', 'checked')
-	}
+    if (data.character) {
+        html = html.replace('data-replace-char', 'value="' + data.character + '"')
+    }
+    if (data.destination) {
+        html = html.replace('data-replace-url', 'value="' + data.destination + '"')
+    }
+    if (data.active) {
+        html = html.replace('data-replace-check', 'checked')
+    }
 
-	e.innerHTML = html;
-	return e.content.firstChild;
+    e.innerHTML = html;
+    return e.content.firstChild;
 }
 
 
 function processFormSubmit(e) {
-	let form = document.getElementById('characterform');
-	let results = [];
+    let form = document.getElementById('characterform');
+    let results = [];
 
-	let sections = form.getElementsByTagName('section');
-	for (var i = 0; i < sections.length; i++) {
-		let section = sections.item(i);
-		let counter = section.dataset.counter;
+    let sections = form.getElementsByTagName('section');
+    for (var i = 0; i < sections.length; i++) {
+        let section = sections.item(i);
+        let counter = section.dataset.counter;
 
-		let character = section.querySelector('[name="character_page-' + counter + '"]').value;
-		let destination = section.querySelector('[name="destination_url-' + counter + '"]').value;
-		let active = section.querySelector('[name="active-' + counter + '"]').checked;
+        let character = section.querySelector('[name="character_page-' + counter + '"]').value;
+        let destination = section.querySelector('[name="destination_url-' + counter + '"]').value;
+        let active = section.querySelector('[name="active-' + counter + '"]').checked;
 
-		let submitted = {
-			character: character,
-			destination: destination,
-			active: active
-		}
-		results.push(submitted)
-	}
+        let submitted = {
+            character: character,
+            destination: destination,
+            active: active
+        }
+        results.push(submitted)
+    }
 
-	chrome.storage.sync.set({
-		data: results
-	});
+    chrome.storage.sync.set({
+        data: results
+    }, function () {
+        chrome.tabs.query({url: "https://www.dndbeyond.com/*"}, function (tabs) {
+            if (tabs.length === 0) {
+                return;
+            }
+            tabs.forEach(function (tab) {
+                console.log(tab);
+                chrome.tabs.sendMessage(tab.id, {data: results}, function (response) {
+                    console.log('Sent message to connected tab.');
+                });
+            });
+        });
+    });
 }
 
 
 function addRemoveListeners(fieldset) {
-	let removeButtons = fieldset.getElementsByClassName('remove-character');
-	for (var i = 0; i < removeButtons.length; i++) {
-		removeButtons.item(i).addEventListener('click', function() {
-			removeCharacterFields(this);
-		})
-	}
+    let removeButtons = fieldset.getElementsByClassName('remove-character');
+    for (var i = 0; i < removeButtons.length; i++) {
+        removeButtons.item(i).addEventListener('click', function () {
+            removeCharacterFields(this);
+        })
+    }
 }
 
 function addCharacterFields(data) {
-	counter++;
-	var newFields = htmlToElement(template, counter, data);
-	newFields.style.display = 'flex';
+    counter++;
+    var newFields = htmlToElement(template, counter, data);
+    newFields.style.display = 'flex';
 
-	var insertHere = document.getElementById('writeroot');
-	insertHere.parentNode.insertBefore(newFields, insertHere);
-	addRemoveListeners(newFields);
+    var insertHere = document.getElementById('writeroot');
+    insertHere.parentNode.insertBefore(newFields, insertHere);
+    addRemoveListeners(newFields);
 }
 
 function removeCharacterFields(e) {
-	e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
+    e.parentNode.parentNode.parentNode.removeChild(e.parentNode.parentNode);
 }
 
 function load() {
-	chrome.storage.sync.get(['data'], function(result) {
-		let data = result.data;
-		data.forEach(function(item, index) {
-			addCharacterFields(item);
-		});
-	});
+    chrome.storage.sync.get(['data'], function (result) {
+        let data = result.data;
+        data.forEach(function (item, index) {
+            addCharacterFields(item);
+        });
+    });
 }
 
 
 function init() {
-	load();
-	document.getElementById('addcharacter').addEventListener("click", function(e) {
-		addCharacterFields();
-	});
-	document.getElementById('characterform').addEventListener("submit", function(e) {
-		return processFormSubmit(e);
-	})
+    load();
+    document.getElementById('addcharacter').addEventListener("click", function (e) {
+        addCharacterFields();
+    });
+    document.getElementById('characterform').addEventListener("submit", function (e) {
+        return processFormSubmit(e);
+    })
 }
 
 
 document.addEventListener('DOMContentLoaded', init, false);
-
 
 
 /*
--- run.js
+++ run.js
@@ -1,4 +1,3 @@
-
 /*
 todo: form to enter webhook
 auto load? and then a button to say connect or disconnect from discord + status on the page itself
@@ -8,78 +7,116 @@ gotta save the connect/disconnect somewhere. disconnect = destroy all observers?
 
 */
 
-var template = `    <form>
-    	<div>
- <label for="character_name">Character Page</label>
-  <input type="text" id="character_name" name="character_name">
- <label for="destination_url">Destination URL</label>
-  <input type="text" id="destination_url" name="destination_url">
-<label for="active">Active</label>
-  <input type="checkbox" id="active" name="active">
-</div>
-  <input type="submit" value="Submit">
+console.log();
 
-    </form>`
-
-console.log('loaded')
-
-let form = document.createElement('div');
-form.innerHTML = template;
-document.body.appendChild(form);
-
-var discordWebhook = 'https://discordapp.com/api/webhooks/717858363832008765/wKAA4IpY6qRT4nwEh8x-6x2VBN64q9IhQlbefZ6bvQeKxxor-yDysCo-_a0J5R7a1BVS';
-
-var diceObserver = new MutationObserver(function(mutations) {
-	mutations.forEach(function(mutation) {
-		if (mutation.addedNodes.length) {
-			var newNode = mutation.addedNodes[0];
-			if (newNode.classList.contains('noty_bar')) {
-				var results = {
-					character: document.getElementsByClassName('ddbc-character-tidbits__name')[0].textContent,
-					rollDetail: newNode.getElementsByClassName('dice_result__info__rolldetail')[0].textContent,
-					rollType: newNode.getElementsByClassName('dice_result__rolltype')[0].textContent,
-					infoBreakdown: newNode.getElementsByClassName('dice_result__info__breakdown')[0].textContent,
-					rolledDice: newNode.getElementsByClassName('dice_result__info__dicenotation')[0].textContent,
-					rolledTotal: newNode.getElementsByClassName('dice_result__total')[0].textContent
-				};
-
-				var msg = results.character + "\n" + results.rollDetail + results.rollType + "\n🎲" + results.infoBreakdown + " = " + results.rolledTotal + "\n" + results.rolledDice;
-
-				//	var xhr = new XMLHttpRequest();
-				//	xhr.open("POST", discordWebhook, true);
-				//	xhr.setRequestHeader('Content-Type', 'application/json');
-				//	xhr.send(JSON.stringify({
-				//		content: msg
-				//	}));
-
-				console.log(msg);
-			}
-		}
-	})
-})
-
-ensureObservation();
-
-var popupObserver = new MutationObserver(function(mutations) {
-	ensureObservation();
-})
-
-popupObserver.observe(document.body, {
-	childList: true
+chrome.storage.sync.get(['data'], function (result) {
+    let data = result.data;
+    data.forEach(function (item, index) {
+        if (item.character === window.location.href) {
+            DnDiscord.run(item.active, item.destination);
+        }
+    });
 });
 
-function ensureObservation() {
-	var dicePopup = document.getElementById('noty_layout__bottomRight');
-	if (dicePopup === null) {
-		dicePopup = document.createElement('div');
-		dicePopup.setAttribute('id', 'noty_layout__bottomRight');
-		dicePopup.setAttribute('role', 'alert');
-		dicePopup.setAttribute('aria-live', 'polite');
-		dicePopup.setAttribute('class', 'noty_layout uncollapse');
-		document.body.appendChild(dicePopup);
-		// Attach the observer
-		diceObserver.observe(dicePopup, {
-			childList: true
-		});
-	}
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        let data = request.data;
+        data.forEach(function (item, index) {
+            if (item.character === window.location.href) {
+                DnDiscord.run(item.active, item.destination);
+            }
+        });
+        return true;
+    });
+
+document.addEventListener('visibilitychange', function() {
+    chrome.storage.sync.get(['data'], function (result) {
+        let data = result.data;
+        data.forEach(function (item, index) {
+            if (item.character === window.location.href) {
+                DnDiscord.run(item.active, item.destination);
+            }
+        });
+    });
+})
+
+
+var DnDiscord = {
+    active: false,
+    destination: null,
+    run: function (active) {
+        var _this = this;
+        console.log('starting run!')
+        this.active = active;
+        this.destination = window.location.href;
+        if (this.active && this.destination && document.visibilityState == 'visible') {
+            console.log('spinning up');
+            this.spinUp();
+        } else {
+            console.log('tearing down')
+            this.teardown();
+        }
+    },
+    diceObserver: new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.addedNodes.length) {
+                let newNode = mutation.addedNodes[0];
+                if (newNode.classList.contains('noty_bar')) {
+                    let results = {
+                        character: document.getElementsByClassName('ddbc-character-tidbits__name')[0].textContent,
+                        rollDetail: newNode.getElementsByClassName('dice_result__info__rolldetail')[0].textContent,
+                        rollType: newNode.getElementsByClassName('dice_result__rolltype')[0].textContent,
+                        infoBreakdown: newNode.getElementsByClassName('dice_result__info__breakdown')[0].textContent,
+                        rolledDice: newNode.getElementsByClassName('dice_result__info__dicenotation')[0].textContent,
+                        rolledTotal: newNode.getElementsByClassName('dice_result__total')[0].textContent
+                    };
+
+                    let msg = results.character + "\n" + results.rollDetail + results.rollType + "\n🎲" + results.infoBreakdown + " = " + results.rolledTotal + "\n" + results.rolledDice;
+
+                    //	var xhr = new XMLHttpRequest();
+                    //	xhr.open("POST", discordWebhook, true);
+                    //	xhr.setRequestHeader('Content-Type', 'application/json');
+                    //	xhr.send(JSON.stringify({
+                    //		content: msg
+                    //	}));
+
+                    console.log(msg);
+                }
+            }
+        })
+    }),
+    popupObserver: new MutationObserver(function (mutations) {
+        DnDiscord.ensureObservation();
+    }),
+    ensureObservation: function ensureObservation() {
+        let dicePopup = document.getElementById('noty_layout__bottomRight');
+        if (dicePopup === null) {
+            dicePopup = document.createElement('div');
+            dicePopup.setAttribute('id', 'noty_layout__bottomRight');
+            dicePopup.setAttribute('role', 'alert');
+            dicePopup.setAttribute('aria-live', 'polite');
+            dicePopup.setAttribute('class', 'noty_layout uncollapse');
+            document.body.appendChild(dicePopup);
+            // Attach the observer
+            this.diceObserver.observe(dicePopup, {
+                childList: true
+            });
+        }
+    },
+    teardown: function () {
+        this.popupObserver.disconnect();
+        this.diceObserver.disconnect();
+        let dicePopup = document.getElementById('noty_layout__bottomRight');
+        dicePopup.parentNode.removeChild(dicePopup);
+    },
+    spinUp: function () {
+        this.ensureObservation();
+        this.popupObserver.observe(document.body, {
+            childList: true
+        });
+    }
 }
+
+
+
